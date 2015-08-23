a8e3e00981ff876f1f334794a1e7fb61f5750408
Petr Vojtechovsky <vojtechovsky@gmail.com>
2015-08-23 16:31:01 +0200

    Initit commit

---
+++ 01-ControllerDomainObjects/App/Simple/SimpleController.ts
@@ -0,0 +1,13 @@
+﻿module App.Simple {
+    class SimpleController {
+
+        title: string;
+
+        constructor() {
+            this.title = "Simple Controller";
+        }
+    }
+
+    angular.module("SimpleModule", [])
+        .controller("SimpleController", SimpleController);
+}
